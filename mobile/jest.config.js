751af6714058a4d275e4417c43858786ff6d4b90
anjb <anjb@ti-net.com.cn>
2026-03-09 09:52:36 +0800

    fix: add Jest setup to mock AsyncStorage
    - Create jest.setup.js to mock @react-native-async-storage
    - Update jest.config.js to use react-native preset
    - Add setupFiles configuration
    - Update transformIgnorePatterns for React Native modules
    
    This fixes the AsyncStorage NativeModule error in Jest tests

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   preset: 'react-native',
+  setupFiles: ['./jest.setup.js'],
   moduleNameMapper: {
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|bak|txt)$': 'identity-obj-proxy',
   },
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-navigation)/)',
+    'node_modules/(?!(react-native|@react-native|@react-navigation|@react-native-async-storage)/)',
   ],
   testEnvironment: 'node',
 };
